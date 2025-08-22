ebe2a6f97c3950a832b70499960738bd37dbf876
Saurabh Yadav <Saurabhyadav831@gmail.com>
2025-08-23 03:51:05 +0545

    Update EmbedPlayer URLs for movie and TV pages

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -6,5 +6,5 @@ export const revalidate = 3600;
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
   const params = await props.params;
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://embed.vidsrc.pk/movie/${id}`} />;
+  return <EmbedPlayer url={`https://player.autoembed.cc/embed/movie/${id}`} />;
 }
