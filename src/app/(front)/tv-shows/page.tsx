b8509662fcfd935aaef1f24a7c1289670dd9f88e
Saurabh Yadav <Saurabhyadav831@gmail.com>
2025-11-02 00:47:17 +0545

    Refactor page component names for consistency and add metadata for Anime, Movies, New & Popular, TV Shows, and their respective slug pages to enhance SEO and user experience.

--- src/app/(front)/tv-shows/page.tsx
+++ src/app/(front)/tv-shows/page.tsx
@@ -6,8 +6,14 @@ import { RequestType, type ShowRequest } from '@/enums/request-type';
 import { getRandomShow } from '@/lib/utils';
 import MovieService from '@/services/MovieService';
 import { MediaType, type Show } from '@/types';
+import { type Metadata } from 'next';
 
 export const revalidate = 3600;
+
+export const metadata: Metadata = {
+  title: 'TV Shows',
+  description: `Browse and watch the latest TV shows on ${siteConfig.name}. Discover trending TV series, popular shows, and shows by genre and streaming platform.`,
+};
 
 export default async function TvShowPage() {
   const h1 = `${siteConfig.name} TV Shows`;
