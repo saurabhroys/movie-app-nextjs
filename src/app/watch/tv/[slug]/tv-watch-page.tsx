5018b8b2662f77cc4fc714a6a5ffaa19d4b624b5
Saurabh Yadav <Saurabhyadav831@gmail.com>
2026-01-12 05:58:44 +0545

    Refactor Tailwind classes and update dependencies
    Updated various Tailwind CSS class names for consistency and to use new syntax (e.g., px-px, h-80, h-160, max-w-232, etc.) across multiple components and pages. Changed some background utility classes and z-index usage. Upgraded Tailwind CSS and related dependencies in package.json to latest versions.

--- src/app/watch/tv/[slug]/tv-watch-page.tsx
+++ src/app/watch/tv/[slug]/tv-watch-page.tsx
@@ -44,14 +44,14 @@ const TvWatchPage = ({
           <PlayerSelector
             mediaId={mediaId}
             mediaType="tv"
-            playerClass="border border-neutral-700 rounded-xl p-[1px]"
-            selectorClass="h-[20rem] md:h-[40rem]"
+            playerClass="border border-neutral-700 rounded-xl p-px"
+            selectorClass="h-80 md:h-160"
             season={selectedSeason}
             episode={selectedEpisode}
           />
         </div>
 
-        <div className="mt-3 h-[35rem] w-full rounded-xl border border-neutral-700 p-1 pt-2 md:col-span-5 md:mt-0 md:mr-2 md:h-[40rem]">
+        <div className="mt-3 h-140 w-full rounded-xl border border-neutral-700 p-1 pt-2 md:col-span-5 md:mt-0 md:mr-2 md:h-160">
           {/* Seasons and Episodes Selector */}
           <div className="flex h-full flex-col">
             {/* <h3 className="text-lg font-semibold text-white mb-0 p-0.5 text-center">Seasons & Episodes</h3> */}
@@ -68,7 +68,7 @@ const TvWatchPage = ({
       </div>
 
       {/* Recommended Movies */}
-      <div className="relative z-10 mt-10 bg-gradient-to-t from-black via-black/80 to-transparent">
+      <div className="relative z-10 mt-10 bg-linear-to-t from-black via-black/80 to-transparent">
         <RecommendedMovies shows={recommendedShows} title="More like this" />
       </div>
     </div>
